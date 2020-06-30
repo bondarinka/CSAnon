3323e6591ad93d1ef4988ec81613e13746be97e1
Daria Bondarenko <bondarinka@Darias-MacBook-Pro.local>
2020-06-30 06:21:09 -0400

    testing of scrolling of the chat feature

--- src/client/components/MainChat.js
+++ src/client/components/MainChat.js
@@ -1,11 +1,24 @@
-import React, { useState, useRef, useContext } from 'react';
-import SocketContext from '../context/SocketContext';
- 
+import React, { useState, useRef, useContext } from "react";
+import SocketContext from "../context/SocketContext";
+import { use } from "../../server/ids/ids";
+
 export default function MainChat(props) {
   const [messageData, setMessageData] = useState([]);
   const inputMessageRef = useRef(null);
   const chatRef = useRef(null);
   const socket = useContext(SocketContext);
+  // TEST !
+  const divRef = useRef(null);
+  // const messageEndRef = useRef(null);
+
+  // const scrollToBottom = () => {
+  //   messageEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  // };
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  });
+  // useEffect(scrollToBottom, [messageData]);
 
   const handleSendClick = (e) => {
     e.preventDefault();
@@ -14,52 +27,64 @@ export default function MainChat(props) {
         message: inputMessageRef.current.value,
         username: props.location.state.username,
       };
-      inputMessageRef.current.value = '';
-      socket.emit('message', data);
+      inputMessageRef.current.value = "";
+      socket.emit("message", data);
     }
   };
 
-  socket.on('newMessage', (data) => {
+  socket.on("newMessage", (data) => {
     //{username, userURL, timestamp, message}
     const newArr = [...messageData];
     newArr.push(data);
     setMessageData(newArr);
     chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    // chatRef.current....?
   });
 
   //TODO: add function for request more messages on scroll up
   //TODO: not scrolling to bottom on initial load
   if (messageData.length === 0) {
-    fetch('/messages/all')
+    fetch("/messages/all")
       .then((res) => res.json())
       .then((res) => {
         setMessageData(res);
       })
-      .then(() => chatRef.current.scrollTop = chatRef.current.scrollHeight);
+      // .then(() => (chatRef.current.scrollTo = chatRef.current.scrollHeight));
+    // .then(() => chatRef.current.scrollTop = chatRef.current.scrollHeight);
   }
 
+  // TEST !
+  // After the Message Component -> // <div ref={messageEndRef} /> ??
+
   return (
-    <div className='mainContainer mainChat'>
-      <div className='sidebar'>
-        <p className='displayName'>{props.location.state.username}</p>
+    <div className="mainContainer mainChat">
+      <div className="sidebar">
+        <p className="displayName">{props.location.state.username}</p>
         <img src={props.location.state.userURL} />
         {/* TODO: add log out functionalities */}
         {/*<button>Log out of GitHub</button>
         <button>Log out of Anon ID</button>*/}
       </div>
-      <div className='chatContainer'>
-        <div className='chat' ref={chatRef}>
+      <div className="chatContainer">
+        <div className="chat" ref={chatRef}>
           {/* assumes most recent message is at the end of the array */}
           {messageData.map((message) => (
-            <Message key={JSON.stringify(message)}
-              yourName={props.location.state.username}
-              {...message} />
-          ))}
+            <Message
+            key={JSON.stringify(message)}
+            yourName={props.location.state.username}
+            {...message}
+            />
+            ))}
+            <div ref={divRef}/>
         </div>
-        <form className='inputArea' onClick={handleSendClick}>
-          <input type='text' ref={inputMessageRef} placeholder='Send message'></input>
+        <form className="inputArea" onClick={handleSendClick}>
+          <input
+            type="text"
+            ref={inputMessageRef}
+            placeholder="Send message"
+          ></input>
           <span>|</span>
-          <button type='submit'>Send</button>
+          <button type="submit">Send</button>
         </form>
       </div>
     </div>
@@ -67,11 +92,12 @@ export default function MainChat(props) {
 }
 
 function Message(props) {
-  const className = 'messageContainer' + (props.yourName === props.username ? ' self' : '');
+  const className =
+    "messageContainer" + (props.yourName === props.username ? " self" : "");
   return (
     <div className={className}>
       <img src={props.userURL} />
-      <div className='textContainer'>
+      <div className="textContainer">
         <span>{props.username}</span>
         <p>{props.message}</p>
       </div>
